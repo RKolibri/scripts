917f860db0dd414bc15be22ad4ce04da8cca1526
Kolibri <klevis@resulaj.eu>
2023-01-28 23:55:21 +0100

    added auto install and some rclone scripts

---
+++ rclone_scripts/sync-dsm-vm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set the source and destination directories
+source="/mydata/proxmox/dump"
+destination="DSM:/4TB/ProxmoxVms"
+
+# Set the log file
+log_file="rclone_sync-dsm-vm.log"
+# Add a timestamp to the log file
+echo "Sync started at $(date)" >> $log_file
+
+# Run the sync command and redirect output to the log file
+rclone sync $source $destination --verbose --log-file=$log_file
+
+# Add a timestamp to the log file
+echo "Sync completed at $(date)" >> $log_file
---
+++ rclone_scripts/sync-ionos-myfiles.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the source and destination directories
+source="/mydata/MyFiles"
+destination="ionos:/users/kolibri96/MyFiles"
+
+# Set the log file
+log_file="rclone_sync_ionos_myfiles.log"
+
+# Add a timestamp to the log file
+echo "Sync started at $(date)" >> $log_file
+
+# Run the sync command and redirect output to the log file
+rclone sync $source $destination --verbose --log-file=$log_file
+
+# Add a timestamp to the log file
+echo "Sync completed at $(date)" >> $log_file
+
