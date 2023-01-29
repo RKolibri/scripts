6e43b516184b0657a185f0d955b2f26403bbd670
Kolibri-Docker <klevis@resulaj.eu>
2023-01-29 12:59:48 +0100

    Updated paths because of repo rename

--- custom_scripts/install_starship.sh
+++ custom_scripts/install_starship.sh
@@ -14,20 +14,20 @@ else
 echo -e "\e[32mStarship already in .bashrc\e[0m"
 fi
 
-echo -e "\e[32mCloning dotfiles repo\e[0m"
-git clone https://github.com/RKolibri/dotfiles ~/dotfiles
+echo -e "\e[32mCloning scripts repo\e[0m"
+git clone https://github.com/RKolibri/scripts.git ~/scripts
 
 echo -e "\e[32mCreating directory if it does not exist\e[0m"
 mkdir -p ~/.config
 
 echo -e "\e[32mCopying files from source to destination\e[0m"
-cp ~/dotfiles/config_folder/starship.toml ~/.config/starship.toml
+cp ~/scripts/config_folder/starship.toml ~/.config/starship.toml
 
 echo -e "\e[32mDisabling password authentication\e[0m"
 sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
 
-echo -e "\e[32mRemoving dotfiles dir\e[0m"
-sudo rm -r ~/dotfiles
+echo -e "\e[32mRemoving scripts dir\e[0m"
+sudo rm -r ~/scripts
 
 exec bash
