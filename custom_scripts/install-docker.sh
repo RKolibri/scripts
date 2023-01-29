255a8185b312526cd4f09592def9025cf139862b
Kolibri-Docker <klevis@resulaj.eu>
2023-01-29 12:27:58 +0100

    updated docker script

--- custom_scripts/install-docker.sh
+++ custom_scripts/install-docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Do you want to install Portainer Community Edition (CE) or Enterprise Edition (EE)?"
+echo -e "\033[32mDo you want to install Portainer Community Edition (CE) or Enterprise Edition (EE)?\033[0m"
 read -p "Enter CE or EE: " edition
 
-# update the package index
+echo -e "\033[32mUpdating the package index...\033[0m"
 sudo apt-get update
 
-# install packages to allow apt to use a repository over HTTPS
+echo -e "\033[32mInstalling packages to allow apt to use a repository over HTTPS...\033[0m"
 sudo apt-get install -y \
   apt-transport-https \
   ca-certificates \
@@ -14,38 +14,39 @@ sudo apt-get install -y \
   gnupg-agent \
   software-properties-common
 
-# add Docker's official GPG key
+echo -e "\033[32mAdding Docker's official GPG key...\033[0m"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-# set up the stable repository
+echo -e "\033[32mSetting up the stable repository...\033[0m"
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
-# update the package index again
+echo -e "\033[32mUpdating the package index again...\033[0m"
 sudo apt-get update
 
-# install Docker CE
+echo -e "\033[32mInstalling Docker CE...\033[0m"
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-# add the current user to the docker group
+echo -e "\033[32mAdding the current user to the docker group...\033[0m"
 sudo usermod -aG docker $USER
 
-# pull the appropriate Portainer image
 if [ "$edition" == "CE" ]; then
-  sudo docker pull portainer/portainer
-  tag="portainer/portainer"
+  echo -e "\033[32mPulling the Portainer Community Edition image...\033[0m"
+  sudo docker pull portainer/portainer-ce
+  tag="portainer/portainer-ce:latest"
 elif [ "$edition" == "EE" ]; then
+  echo -e "\033[32mPulling the Portainer Enterprise Edition image...\033[0m"
   sudo docker pull portainer/portainer-ee
   tag="portainer/portainer-ee:latest"
 else
-  echo "Invalid option. Exiting."
+  echo -e "\033[31mInvalid option. Exiting.\033[0m"
   exit 1
 fi
 
-# create a data volume for Portainer
+echo -e "\033[32mCreating a data volume for Portainer...\033[0m"
 sudo docker volume create portainer_data
 
-# run a Portainer container
+echo -e "\033[32mRunning a Portainer container...\033[0m"
 sudo docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data $tag
